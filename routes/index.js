a1d59d506bd2573205aea3a0f2ff23e0c846d3dc
CrissCorre2024 <cristiancorredorcuestas@cedoc.edu.co>
2024-07-18 21:23:21 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,200 +1,156 @@
-var utils = require('../utils');
-var mongoose = require('mongoose');
-var Todo = mongoose.model('Todo');
-var User = mongoose.model('User');
-// TODO:
-var hms = require('humanize-ms');
-var ms = require('ms');
-var streamBuffers = require('stream-buffers');
-var readline = require('readline');
-var moment = require('moment');
-var exec = require('child_process').exec;
-var validator = require('validator');
+const utils = require('../utils');
+const mongoose = require('mongoose');
+const Todo = mongoose.model('Todo');
+const User = mongoose.model('User');
+const hms = require('humanize-ms');
+const ms = require('ms');
+const streamBuffers = require('stream-buffers');
+const readline = require('readline');
+const moment = require('moment');
+const exec = require('child_process').exec;
+const validator = require('validator');
+const fileType = require('file-type');
+const AdmZip = require('adm-zip');
+const fs = require('fs');
+const _ = require('lodash');
 
-// zip-slip
-var fileType = require('file-type');
-var AdmZip = require('adm-zip');
-var fs = require('fs');
-
-// prototype-pollution
-var _ = require('lodash');
-
-exports.index = function (req, res, next) {
-  Todo.
-    find({}).
-    sort('-updated_at').
-    exec(function (err, todos) {
-      if (err) return next(err);
-
-      res.render('index', {
-        title: 'Patch TODO List',
-        subhead: 'Vulnerabilities at their best',
-        todos: todos,
-      });
+exports.index = (req, res, next) => {
+  Todo.find({}).sort('-updated_at').exec((err, todos) => {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'Patch TODO List',
+      subhead: 'Vulnerabilities at their best',
+      todos,
     });
+  });
 };
 
-exports.loginHandler = function (req, res, next) {
-  if (validator.isEmail(req.body.username)) {
-    // Cambia find por findOne para evitar inyecciones NoSQL
-    User.findOne({ username: req.body.username, password: req.body.password }, function (err, user) {
+exports.loginHandler = (req, res, next) => {
+  const { username, password, redirectPage } = req.body;
+
+  if (validator.isEmail(username)) {
+    User.findOne({ username, password }, (err, user) => {
       if (err) return next(err);
       if (user) {
-        const redirectPage = req.body.redirectPage;
-        const session = req.session;
-        const username = req.body.username;
-        return adminLoginSuccess(redirectPage, session, username, res);
+        adminLoginSuccess(redirectPage, req.session, username, res);
       } else {
-        return res.status(401).send();
+        res.status(401).send();
       }
     });
   } else {
-    return res.status(401).send();
+    res.status(401).send();
   }
 };
 
 function adminLoginSuccess(redirectPage, session, username, res) {
-  session.loggedIn = 1
-
-  // Log the login action for audit
-  console.log(`User logged in: ${username}`)
-
-  if (redirectPage) {
-      return res.redirect(redirectPage)
-  } else {
-      return res.redirect('/admin')
-  }
+  session.loggedIn = 1;
+  console.log(`User logged in: ${username}`);
+  res.redirect(redirectPage || '/admin');
 }
 
-exports.login = function (req, res, next) {
-  return res.render('admin', {
+exports.login = (req, res, next) => {
+  res.render('admin', {
     title: 'Admin Access',
     granted: false,
-    redirectPage: req.query.redirectPage
+    redirectPage: req.query.redirectPage,
   });
 };
 
-exports.admin = function (req, res, next) {
-  return res.render('admin', {
+exports.admin = (req, res, next) => {
+  res.render('admin', {
     title: 'Admin Access Granted',
     granted: true,
   });
 };
 
-exports.get_account_details = function(req, res, next) {
-  // @TODO need to add a database call to get the profile from the database
-  // and provide it to the view to display
-  const profile = {}
- 	return res.render('account.hbs', profile)
-}
+exports.get_account_details = (req, res, next) => {
+  // Placeholder for getting user profile from the database
+  const profile = {};
+  res.render('account.hbs', profile);
+};
 
-exports.save_account_details = function(req, res, next) {
-  // get the profile details from the JSON
-	const profile = req.body
-  // validate the input
-  if (validator.isEmail(profile.email, { allow_display_name: true })
-    // allow_display_name allows us to receive input as:
-    // Display Name <email-address>
-    // which we consider valid too
-    && validator.isMobilePhone(profile.phone, 'he-IL')
-    && validator.isAscii(profile.firstname)
-    && validator.isAscii(profile.lastname)
-    && validator.isAscii(profile.country)
-  ) {
-    // trim any extra spaces on the right of the name
-    profile.firstname = validator.rtrim(profile.firstname)
-    profile.lastname = validator.rtrim(profile.lastname)
+exports.save_account_details = (req, res, next) => {
+  const profile = req.body;
 
-    // render the view
-    return res.render('account.hbs', profile)
+  if (validator.isEmail(profile.email, { allow_display_name: true }) &&
+      validator.isMobilePhone(profile.phone, 'he-IL') &&
+      validator.isAscii(profile.firstname) &&
+      validator.isAscii(profile.lastname) &&
+      validator.isAscii(profile.country)) {
+
+    profile.firstname = validator.rtrim(profile.firstname);
+    profile.lastname = validator.rtrim(profile.lastname);
+
+    res.render('account.hbs', profile);
   } else {
-    // if input validation fails, we just render the view as is
-    console.log('error in form details')
-    return res.render('account.hbs')
+    console.log('Error in form details');
+    res.render('account.hbs');
   }
-}
+};
 
-exports.isLoggedIn = function (req, res, next) {
+exports.isLoggedIn = (req, res, next) => {
   if (req.session.loggedIn === 1) {
-    return next()
+    next();
   } else {
-    return res.redirect('/')
+    res.redirect('/');
   }
-}
+};
 
-exports.logout = function (req, res, next) {
-  req.session.loggedIn = 0
-  req.session.destroy(function() { 
-    return res.redirect('/')  
-  })
-}
+exports.logout = (req, res, next) => {
+  req.session.loggedIn = 0;
+  req.session.destroy(() => res.redirect('/'));
+};
 
 function parse(todo) {
-  var t = todo;
+  const remindToken = ' in ';
+  const reminderIndex = todo.indexOf(remindToken);
+  
+  if (reminderIndex > 0) {
+    let time = todo.slice(reminderIndex + remindToken.length).trim();
+    const period = hms(time);
 
-  var remindToken = ' in ';
-  var reminder = t.toString().indexOf(remindToken);
-  if (reminder > 0) {
-    var time = t.slice(reminder + remindToken.length);
-    time = time.replace(/\n$/, '');
+    console.log('Period:', period);
 
-    var period = hms(time);
-
-    console.log('period: ' + period);
-
-    // remove it
-    t = t.slice(0, reminder);
-    if (typeof period != 'undefined') {
-      t += ' [' + ms(period) + ']';
+    todo = todo.slice(0, reminderIndex);
+    if (period) {
+      todo += ` [${ms(period)}]`;
     }
   }
-  return t;
+  
+  return todo;
 }
 
-exports.create = function (req, res, next) {
-  // console.log('req.body: ' + JSON.stringify(req.body));
+exports.create = (req, res, next) => {
+  const item = req.body.content;
+  const imgRegex = /\!\[alt text\]\((http.*)\s\".*/;
 
-  var item = req.body.content;
-  var imgRegex = /\!\[alt text\]\((http.*)\s\".*/;
-  if (typeof (item) == 'string' && item.match(imgRegex)) {
-    var url = item.match(imgRegex)[1];
-    console.log('found img: ' + url);
+  if (typeof item === 'string' && imgRegex.test(item)) {
+    const url = item.match(imgRegex)[1];
+    console.log('Found img:', url);
 
-    exec('identify ' + url, function (err, stdout, stderr) {
-      console.log(err);
-      if (err !== null) {
-        console.log('Error (' + err + '):' + stderr);
+    exec(`identify ${url}`, (err, stdout, stderr) => {
+      if (err) {
+        console.log('Error:', err, stderr);
       }
     });
-
   } else {
-    item = parse(item);
+    req.body.content = parse(item);
   }
 
   new Todo({
-    content: item,
+    content: req.body.content,
     updated_at: Date.now(),
-  }).save(function (err, todo, count) {
+  }).save((err, todo) => {
     if (err) return next(err);
-
-    /*
-    res.setHeader('Data', todo.content.toString('base64'));
-    res.redirect('/');
-    */
-
-    res.setHeader('Location', '/');
-    res.status(302).send(todo.content.toString('base64'));
-
-    // res.redirect('/#' + todo.content.toString('base64'));
+    res.status(302).redirect('/');
   });
 };
 
-exports.destroy = function (req, res, next) {
-  // Usa findById para buscar el todo
-  Todo.findById(req.params.id, function (err, todo) {
+exports.destroy = (req, res, next) => {
+  Todo.findById(req.params.id, (err, todo) => {
     if (err) return next(err);
     if (todo) {
-      todo.remove(function (err) {
+      todo.remove((err) => {
         if (err) return next(err);
         res.redirect('/');
       });
@@ -204,28 +160,24 @@ exports.destroy = function (req, res, next) {
   });
 };
 
-exports.edit = function (req, res, next) {
-  Todo.
-    find({}).
-    sort('-updated_at').
-    exec(function (err, todos) {
-      if (err) return next(err);
-
-      res.render('edit', {
-        title: 'TODO',
-        todos: todos,
-        current: req.params.id
-      });
+exports.edit = (req, res, next) => {
+  Todo.find({}).sort('-updated_at').exec((err, todos) => {
+    if (err) return next(err);
+    res.render('edit', {
+      title: 'TODO',
+      todos,
+      current: req.params.id,
     });
+  });
 };
 
-exports.update = function (req, res, next) {
-  Todo.findById(req.params.id, function (err, todo) {
+exports.update = (req, res, next) => {
+  Todo.findById(req.params.id, (err, todo) => {
     if (err) return next(err);
     if (todo) {
       todo.content = req.body.content;
       todo.updated_at = Date.now();
-      todo.save(function (err) {
+      todo.save((err) => {
         if (err) return next(err);
         res.redirect('/');
       });
@@ -235,104 +187,83 @@ exports.update = function (req, res, next) {
   });
 };
 
-// ** express turns the cookie key to lowercase **
-exports.current_user = function (req, res, next) {
-
+exports.current_user = (req, res, next) => {
   next();
 };
 
 function isBlank(str) {
-  return (!str || /^\s*$/.test(str));
+  return !str || /^\s*$/.test(str);
 }
 
-exports.import = function (req, res, next) {
-  if (!req.files) {
+exports.import = (req, res, next) => {
+  if (!req.files || !req.files.importFile) {
     res.send('No files were uploaded.');
     return;
   }
 
-  var importFile = req.files.importFile;
-  var data;
-  var importedFileType = fileType(importFile.data);
-  var zipFileExt = { ext: "zip", mime: "application/zip" };
-  if (importedFileType === null) {
-    importedFileType = { ext: "txt", mime: "text/plain" };
-  }
-  if (importedFileType["mime"] === zipFileExt["mime"]) {
-    var zip = AdmZip(importFile.data);
-    var extracted_path = "/tmp/extracted_files";
-    zip.extractAllTo(extracted_path, true);
-    data = "No backup.txt file found";
-    fs.readFile('backup.txt', 'ascii', function (err, data) {
-      if (!err) {
-        data = data;
-      }
+  const importFile = req.files.importFile;
+  let data;
+  const importedFileType = fileType(importFile.data) || { ext: 'txt', mime: 'text/plain' };
+
+  if (importedFileType.mime === 'application/zip') {
+    const zip = new AdmZip(importFile.data);
+    const extractedPath = '/tmp/extracted_files';
+    zip.extractAllTo(extractedPath, true);
+    fs.readFile(`${extractedPath}/backup.txt`, 'ascii', (err, fileData) => {
+      data = err ? 'No backup.txt file found' : fileData;
+      processImportData(data, res);
     });
   } else {
     data = importFile.data.toString('ascii');
+    processImportData(data, res);
   }
-  var lines = data.split('\n');
-  lines.forEach(function (line) {
-    var parts = line.split(',');
-    var what = parts[0];
-    console.log('importing ' + what);
-    var when = parts[1];
-    var locale = parts[2];
-    var format = parts[3];
-    var item = what;
-    if (!isBlank(what)) {
-      if (!isBlank(when) && !isBlank(locale) && !isBlank(format)) {
-        console.log('setting locale ' + parts[1]);
-        moment.locale(locale);
-        var d = moment(when);
-        console.log('formatting ' + d);
-        item += ' [' + d.format(format) + ']';
-      }
+};
 
+function processImportData(data, res) {
+  const lines = data.split('\n');
+  lines.forEach((line) => {
+    const [what, when, locale, format] = line.split(',');
+    if (!isBlank(what)) {
+      let item = what;
+      if (!isBlank(when) && !isBlank(locale) && !isBlank(format)) {
+        moment.locale(locale);
+        const d = moment(when);
+        item += ` [${d.format(format)}]`;
+      }
       new Todo({
         content: item,
         updated_at: Date.now(),
-      }).save(function (err, todo, count) {
-        if (err) return next(err);
-        console.log('added ' + todo);
+      }).save((err) => {
+        if (err) console.log('Error adding todo:', err);
       });
     }
   });
-
   res.redirect('/');
-};
+}
 
-exports.about_new = function (req, res, next) {
-  console.log(JSON.stringify(req.query));
-  return res.render("about_new.dust",
-    {
-      title: 'Patch TODO List',
-      subhead: 'Vulnerabilities at their best',
-      device: req.query.device
-    });
+exports.about_new = (req, res, next) => {
+  console.log('Query:', JSON.stringify(req.query));
+  res.render('about_new.dust', {
+    title: 'Patch TODO List',
+    subhead: 'Vulnerabilities at their best',
+    device: req.query.device,
+  });
 };
 
 // Prototype Pollution
-
-///////////////////////////////////////////////////////////////////////////////
-// In order of simplicity we are not using any database. But you can write the
-// same logic using MongoDB.
 const users = [
-  // You know password for the user.
   { name: 'user', password: 'pwd' },
-  // You don't know password for the admin.
   { name: 'admin', password: Math.random().toString(32), canDelete: true },
 ];
 
-let messages = [];
+const messages = [];
 let lastId = 1;
 
 function findUser(auth) {
-  return users.find((u) =>
+  return users.find(u =>
     u.name === auth.name &&
     u.password === auth.password);
 }
-///////////////////////////////////////////////////////////////////////////////
 
 exports.chat = {
   get(req, res) {
@@ -340,35 +271,28 @@ exports.chat = {
   },
   add(req, res) {
     const user = findUser(req.body.auth || {});
-
     if (!user) {
       res.status(403).send({ ok: false, error: 'Access denied' });
       return;
     }
-
     const message = {
-      // Default message icon. Cen be overwritten by user.
       icon: '👋',
-    };
-
-    _.merge(message, req.body.message, {
+      ...req.body.message,
       id: lastId++,
       timestamp: Date.now(),
       userName: user.name,
-    });
-
+    };
     messages.push(message);
     res.send({ ok: true });
   },
   delete(req, res) {
     const user = findUser(req.body.auth || {});
-
     if (!user || !user.canDelete) {
       res.status(403).send({ ok: false, error: 'Access denied' });
       return;
     }
-
-    messages = messages.filter((m) => m.id !== req.body.messageId);
+    const messageId = req.body.messageId;
+    messages = messages.filter(m => m.id !== messageId);
     res.send({ ok: true });
-  }
+  },
 };
